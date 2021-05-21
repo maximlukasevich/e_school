35a35e8bc5827f942ac50b313bca8abf2fb8893a
maximlukasevich <maxim.lukasevich@gmail.com>
2021-05-21 14:06:05 +0300

    Added models for: Class, Grades, Lessons, Schedule and Users

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,13 +1,13 @@
-const ClassModel = require('./Class')
-const GradesModel = require('./Grades')
-const LessonModel = require('./Lesson')
-const ScheduleModel = require('./Schedule')
-const UserModel = require('./User')
+const Class = require('./Class')
+const Grades = require('./Grades')
+const Lesson = require('./Lesson')
+const Schedule = require('./Schedule')
+const User = require('./User')
 
 module.exports = {
-    ClassModel,
-    GradesModel,
-    LessonModel,
-    ScheduleModel,
-    UserModel
+    Class,
+    Grades,
+    Lesson,
+    Schedule,
+    User
 }
