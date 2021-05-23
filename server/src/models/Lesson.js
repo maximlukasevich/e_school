4c62fa7dd288f59bb8848e9d1e0f6f176ee9044f
maximlukasevich <maxim.lukasevich@gmail.com>
2021-05-23 20:02:14 +0300

    Added "slug" field

--- server/src/models/Lesson.js
+++ server/src/models/Lesson.js
@@ -1,7 +1,8 @@
 const {Schema, model} = require('mongoose')
 
 const Lesson = new Schema({
-    name: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
+    slug: {type: String, required: true},
     teachers: [{type: Schema.Types.ObjectId, ref: 'User'}],
     classes: [{type: Schema.Types.ObjectId, ref: 'Class'}]
 })
