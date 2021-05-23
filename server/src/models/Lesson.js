9ffac5513d53853fc6dc22b7244eafc63cf9fb93
maximlukasevich <maxim.lukasevich@gmail.com>
2021-05-23 20:17:45 +0300

    Added "slug" field and slug generator

--- server/src/models/Lesson.js
+++ server/src/models/Lesson.js
@@ -1,8 +1,11 @@
-const {Schema, model} = require('mongoose')
+const {Schema, model, mongoose} = require('mongoose')
+const slug = require('mongoose-slug-generator')
+
+mongoose.plugin(slug)
 
 const Lesson = new Schema({
     name: {type: String, required: true},
-    slug: {type: String, required: true},
+    slug: {type: String, slug: 'name'},
     teachers: [{type: Schema.Types.ObjectId, ref: 'User'}],
     classes: [{type: Schema.Types.ObjectId, ref: 'Class'}]
 })
