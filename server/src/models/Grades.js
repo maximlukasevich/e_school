93b01bef5bce1d1a12bb04eaa3bb0b41724f2e31
maximlukasevich <maxim.lukasevich@gmail.com>
2021-05-23 19:24:15 +0300

    Multiple fields changed

--- server/src/models/Grades.js
+++ server/src/models/Grades.js
@@ -2,8 +2,10 @@ const {Schema, model} = require('mongoose')
 
 const Grades = new Schema({
     lesson: {type: Schema.Types.ObjectId, ref: 'Lesson'},
-    day: {type: Date, required: true},
-    value: {type: Number, required: true}
+    grades: [{
+       day: {type: Date, required: true},
+       value: {type: Number, required: true}
+    }]
 })
 
 module.exports = new model('Grades', Grades)
--- server/src/models/Lesson.js
+++ server/src/models/Lesson.js
@@ -3,6 +3,7 @@ const {Schema, model} = require('mongoose')
 const Lesson = new Schema({
     name: {type: String, required: true, unique: true},
     teachers: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    classes: [{type: Schema.Types.ObjectId, ref: 'Class'}]
 })
 
 module.exports = new model('Lesson', Lesson)
