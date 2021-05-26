f1f4833464c8480f257d476c14ea21149d745375
maximlukasevich <maxim.lukasevich@gmail.com>
2021-05-26 16:43:29 +0300

    Client init

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
-import './App.css'
-import NavbarPanel from "./components/NavbarPanel/NavbarPanel";
-import Login from "./components/Auth/Login/Login";
-import Registration from "./components/Auth/Registration/Registration";
+import './App.css';
 
 function App() {
     return (
         <div>
             <h1>app.js</h1>
-            <NavbarPanel auth={true} role={'Адмін'} username={'maxim.lukasevich@gmail.com'}/>
-            <Login />
-            <Registration />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
