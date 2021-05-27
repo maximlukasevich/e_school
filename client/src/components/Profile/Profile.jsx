2208e0288ef4e1754c81f2235f2a53f48b058825
maximlukasevich <maxim.lukasevich@gmail.com>
2021-05-27 16:04:09 +0300

    Added Profile component

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -4,10 +4,35 @@ import './profile.css'
 const Profile = (props) => {
     return (
         <div className="container">
-            <div className="profile-header">
-                <h2>{props.username} Лукасевич Максим Анатолійович</h2>
-            </div>
-            <div className="main-content">
+            <div className="profile">
+                <div className="main-info">
+                    <div className="main-info-header">
+                        <h2>{props.user}</h2>
+                        <p>{props.role}
+                            {(props.role === 'Учень') && ' | Класс ' + props.class}
+                        </p>
+                    </div>
+                </div>
+
+                <div className="side-info">
+                    <div className="block">
+                        <h3>Контакти</h3>
+                        <ul>
+                            <li>Email <p>{props.email}</p></li>
+                            <li>Телефон <p>{props.phone}</p></li>
+                        </ul>
+                    </div>
+
+                    <div className="block">
+                        <h3>Адреса</h3>
+                        <ul>
+                            <li>Місто <br/><p>{props.city}</p></li>
+                            <li>Вулиця <br/><p>{props.street}</p></li>
+                            <li>Будинок <br/><p>{props.apartments}</p></li>
+                            <li>Індекс <br/><p>{props.zipcode}</p></li>
+                        </ul>
+                    </div>
+                </div>
 
             </div>
         </div>
