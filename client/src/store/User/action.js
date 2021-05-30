e4a554341aa73a652bad580d9d1a80af44566fd0
maximlukasevich <maxim.lukasevich@gmail.com>
2021-05-30 20:36:05 +0300

    Added login, registration, auth, update functions

---
+++ client/src/store/User/action.js
@@ -0,0 +1,93 @@
+/* eslint-disable no-unreachable */
+import axios from "axios";
+import {setUser} from "./reducer";
+
+export const login = (email, password) => {
+    try {
+        return async (dispatch) => {
+            const res = await axios.post('http://localhost:5000/api/auth/login', {
+                email,
+                password
+            })
+            alert(res.data.message)
+            dispatch(setUser(res.data.user))
+            localStorage.setItem('token', res.data.token)
+        }
+    } catch (err) {
+        alert('Невірний логін та\\або пароль')
+        console.log(err)
+    }
+}
+
+export const registration = (firstName, lastName, middleName, email, password, role) => {
+    try {
+        return async (dispatch) => {
+            try {
+                const res = await axios.post('http://localhost:5000/api/auth/registration', {
+                    firstName,
+                    lastName,
+                    middleName,
+                    email,
+                    password,
+                    role
+                })
+                alert(res.data.message)
+                dispatch(setUser(res.data.user))
+                localStorage.setItem('token', res.data.token)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const update = (
+    firstName, lastName, middleName,
+    phone, gender, city,
+    street, apartments, zipCode,
+    birthday, userId) => {
+    try {
+        return async (dispatch) => {
+            const res = await axios.put(`http://localhost:5000/api/users/${userId}`, {
+                firstName,
+                lastName,
+                middleName,
+                phone,
+                gender,
+                city,
+                street,
+                apartments,
+                zipCode,
+            }, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            alert(res.data.message)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const auth = () => {
+    try {
+        return async (dispatch) => {
+            const res = await axios.get('http://localhost:5000/api/auth/auth', {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            dispatch(setUser(res.data.user))
+            localStorage.setItem('token', res.data.token)
+        }
+    } catch (err) {
+        console.log(err)
+        localStorage.removeItem('token')
+    }
+}
+
+
+
